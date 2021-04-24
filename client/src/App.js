aa75fa677044a58528d64d6bc8f4b8e50eaaf2a9
Wendy Uyen Vo <uyen199247@gmail.com>
2021-04-25 00:35:46 +1000

    added responsive layout and semantic ui packages

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import FavoritesList from "./pages/FavoritesList";
 
@@ -12,13 +12,13 @@ function App() {
     <Router>
       <div>
         <StoreProvider>
-          <Nav />
+          {/* <Nav /> */}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
-            <Route exact path="/favorites" component={FavoritesList} />
+            {/* <Route exact path="/favorites" component={FavoritesList} />
             <Route exact path="/posts/:id" component={Detail} />
-            <Route component={NoMatch} />
+            <Route component={NoMatch} /> */}
           </Switch>
         </StoreProvider>
       </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import CreatePostForm from "../components/CreatePostForm";
-import PostsList from "../components/PostsList";
+import ResponsiveContainer from "../components/ResponsiveLayout";
+// import { Col, Row, Container } from "../components/Grid";
+// import CreatePostForm from "../components/CreatePostForm";
+// import PostsList from "../components/PostsList";
 
 const Home = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-6">
-          <CreatePostForm />
-        </Col>
-        <Col size="md-6 sm-12">
-          <PostsList />
-        </Col>
-      </Row>
-    </Container>
+    <ResponsiveContainer />
   );
 };
 
