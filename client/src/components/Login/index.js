b1b3e64c1b0fc56f10e7fd19eeca4b23cf600bab
Wendy Uyen Vo <uyen199247@gmail.com>
2021-04-29 21:11:59 +1000

    created Login component

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Message,
+    Segment
+  } from "semantic-ui-react";
+
+class Login extends Component {
+  render() {
+    return (
+<Grid textAlign="center" verticalAlign="middle">
+<Grid.Column style={{ maxWidth: 450 }}>
+  <Header as="h2" color="teal" textAlign="center">
+    <img src="/static/images/logo.png" alt="logo" className="image" />{" "}
+    Log-in to your account
+  </Header>
+  <Form size="large">
+    <Segment stacked>
+      <Form.Input
+        fluid
+        icon="user"
+        iconPosition="left"
+        placeholder="E-mail address"
+      />
+      <Form.Input
+        fluid
+        icon="lock"
+        iconPosition="left"
+        placeholder="Password"
+        type="password"
+      />
+      <Button color="teal" fluid size="large">
+        Login
+      </Button>
+    </Segment>
+  </Form>
+  <Message>
+    New to us? <a href="#root">Sign Up</a>
+  </Message>
+</Grid.Column>
+</Grid>
+  
+    );
+};
+};
+
+export default Login;
